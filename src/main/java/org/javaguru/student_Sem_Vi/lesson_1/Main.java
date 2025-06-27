0e8d48218b3920bfb8930e125d3f2c7e4d46d5f1
Viikklly <94692215+Viikklly@users.noreply.github.com>
2025-06-27 11:29:42 +0000

    lesson_1: level_5, level_6 task_1,2

---
+++ src/main/java/org/javaguru/student_Sem_Vi/lesson_1/Main.java
@@ -0,0 +1,14 @@
+package org.javaguru.student_Sem_Vi.lesson_1;
+
+public class Main {
+    public static void main(String[] args) {
+        // level_5
+        System.out.println("Hello World!");
+
+        //level_6 Task_1
+        System.out.println(74 + 36);
+
+        //level_6 Task_2
+        System.out.println(3 * 50);
+    }
+}
